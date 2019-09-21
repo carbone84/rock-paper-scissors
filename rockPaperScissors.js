ab07e27978952a7ad644d7aa4315a749359d7efd
Bryan <bryanacarbone@gmail.com>
2019-09-21 10:51:05 -0400

    added to html, css, and js files to convert to web version

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,43 +1,61 @@
 var rounds = 5;
-var playerSelection;
 var playerScore = 0;
 var computerScore = 0;
-var choices = ['rock', 'paper', 'scissors'];
+var choices = document.querySelectorAll(".choice");
+var playerSelection_span = document.querySelector(".playerSelection");
+var computerSelection_span = document.querySelector(".computerSelection");
+var winLoseDraw_span = document.querySelector(".winLoseDraw");
+var playerScore_span = document.querySelector(".playerScore");
+var computerScore_span = document.querySelector(".computerScore");
+var reset_button = document.querySelector(".reset");
 
-function computerPlay () {
-    return choices[Math.floor(Math.random() * 3)];
+init();
+
+function init() {
+    setupChoices();
+}
+
+// add click listeners to r,p,s buttons
+function setupChoices() {
+    for(var i = 0; i < choices.length; i++) {
+        choices[i].addEventListener("click", function() {
+            playRound(this.id, computerPlay());
+        });
+    }
+}
+
+function computerPlay() {
+    return choices[Math.floor(Math.random() * 3)].id;
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log("Player: " + playerSelection + " Computer: " + computerSelection);
+    playerSelection_span.textContent = playerSelection;
+    computerSelection_span.textContent = computerSelection;
     if (playerSelection.toLowerCase() === computerSelection) {
-        return "TIE! " + playerSelection + " = " + computerSelection;
+        winLoseDraw_span.textContent = "It's a DRAW!";
     } else if ((playerSelection.toLowerCase() === "rock" && computerSelection === "scissors")
             || (playerSelection.toLowerCase() === "paper" && computerSelection === "rock")
             || (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper")) {
         playerScore++;
-        return "You WIN! " + playerSelection + " beats " + computerSelection + "!";
+        playerScore_span.textContent = playerScore;
+        winLoseDraw_span.textContent = "YOU WON!! =)"
     } else {
         computerScore++;
-        return "You lose. " + computerSelection + " beats " + playerSelection + ".";
+        computerScore_span.textContent = computerScore;
+        winLoseDraw_span.textContent = "You lost. =(";
     }
 }
 
-function game() {
-    for(var i = 1; i <= rounds; i++) {
-        playerSelection = "";
-        while(!choices.includes(playerSelection.toLowerCase())) {
-            playerSelection = prompt("Round " + i + ": Rock, Paper, Scissors?");
-        }
-        console.log(playRound(playerSelection, computerPlay()));
-    }
-    console.log("Player score: " + playerScore);
-    console.log("Computer score: " + computerScore);
-    if (playerScore > computerScore) {
-        return "You BEAT the computer!";
-    } else if (computerScore > playerScore) {
-        return "The computer beat you. =(";
-    } else {
-        return "It was a tie. =| ";
-    }
+reset_button.addEventListener("click", function() {
+    reset();
+})
+
+function reset() {
+    playerScore = 0;
+    playerScore_span.textContent = playerScore;
+    computerScore = 0;
+    computerScore_span.textContent = computerScore;
+    winLoseDraw_span.textContent = "Make a selection!"
+    playerSelection_span.textContent = "";
+    computerSelection_span.textContent = "";
 }
